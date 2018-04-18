98ed510dd2a705ee19895953529e9c1f8d877bdb
Walter Devia <walter.devia@outlook.com>
2018-04-18 10:31:33 -0500

    day 13 completed

---
+++ lib/filter/doctrine/sfDoctrineGuardPlugin/sfGuardPermissionFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sfGuardPermission filter form.
+ *
+ * @package    jobee
+ * @subpackage filter
+ * @author     Walter Devia <walter.devia@outlook.com>
+ * @version    SVN: $Id: sfDoctrinePluginFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfGuardPermissionFormFilter extends PluginsfGuardPermissionFormFilter
+{
+  public function configure()
+  {
+  }
+}
